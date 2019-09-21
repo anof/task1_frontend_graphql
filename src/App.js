c02ed1d7ce1e4287d9e21a38781978aaf6fae593
anof <anof1998@gmail.com>
2019-09-21 17:26:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios'
+
+
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData() {
+    axios({
+      url: 'http://localhost:8000/graphql/',
+      method: 'post',
+      data: {
+        query: `
+        {
+          allMusic{
+            albums
+            songs
+            artists
+          }
+        }
+          `
+      }
+    }).then((response) => {
+      this.setMusicData(response.data.data.allMusic[0]);
+
+    }).catch((error)=>{
+      console.log(error)
+      document.getElementById('header').innerHTML="Api not answering"
+    });
+  };
+
+  setMusicData(response)
+  {
+    let albums = response.albums.split(',');
+    let artists = response.artists.split(',');
+    let songs = response.songs.split(',');
+
+    let albumsElement = document.getElementById('albums');
+    let artistsElement = document.getElementById('artists');
+    let songsElement = document.getElementById('songs');
+
+
+    albums.forEach(album => {
+      let temp = document.createElement('li');
+      temp.innerHTML = "<li>" + album + "</li>";
+      albumsElement.appendChild(temp)
+    });
+
+
+    artists.forEach(artist => {
+      let temp = document.createElement('li');
+      temp.innerHTML = "<li>" + artist + "</li>";
+      artistsElement.appendChild(temp)
+    });
+
+    songs.forEach(song => {
+      let temp = document.createElement('li');
+      temp.innerHTML = "<li>" + song + "</li>";
+      songsElement.appendChild(temp)
+    });
+  }
+
+  
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" id="header">
+          <div>
+            Albums
+              <ul id="albums">
+
+            </ul>
+          </div>
+          <div>
+            Artists
+              <ul id="artists">
+
+            </ul>
+          </div>
+          <div>
+            Songs
+              <ul id="songs">
+
+            </ul>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
